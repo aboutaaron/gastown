704a0c11e93fdb197a269eb2031dbf8aaeb1feed
aboutaaron <aaron.colby.williams@gmail.com>
2013-05-31 12:25:46 -0700

    got leaflet up and running with stamen tiles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,16 @@
 jQuery(document).ready(function($) {
-    console.log("Running jQuery " + $.fn.jquery);
+    'use strict';
+
+    var App = App || {};
+
+    // Setup Map
+    App.layer = new L.StamenTileLayer("toner");
+    App.map = new L.Map("map", {
+        center: new L.LatLng(49.261226, -123.113927),
+        zoom: 12
+    });
+
+    App.map.addLayer(App.layer);
+
+
 });
