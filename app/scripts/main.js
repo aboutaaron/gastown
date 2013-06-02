27590811f6825e39e8231d530f08b0cbae1f0f64
aboutaaron <aaron.colby.williams@gmail.com>
2013-06-02 14:01:58 -0700

    Created new global object Data to keep track of the math I'm using for the app.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,14 @@
 // Creating an object as to not pollute the global namesapce
-var App = App || {};
+var Data = Data || {};
+// Some Default info
+Data.bingKey = "AnhYJU1pFXh6M5Qn4zg4htWejzdN5VKZ_c8CC0MQdsCNHaxz-JzNbmwfsMyx3bDq";
+Data.totalUnits = 0;
+Data.totalOutstanding = 0;
+Data.averageUnit = 0;
+Data.averageOutstanding = 0;
 
 // Why is this a global?
-App.markers = new L.MarkerClusterGroup({
+var markers = new L.MarkerClusterGroup({
     showCoverageOnHover: false,
     disableClusteringAtZoom: 16
 });
@@ -20,11 +26,11 @@ function GeocodeCallback(result) {
             color: 'red'
         });
 
-        App.marker = L.marker(result.resourceSets[0].resources[0].point.coordinates, { icon: redMarker });
-        App.marker.bindPopup(result.resourceSets[0].resources[0].name);
+        var marker = L.marker(result.resourceSets[0].resources[0].point.coordinates, { icon: redMarker });
+        marker.bindPopup(result.resourceSets[0].resources[0].name);
         // Add the coordinates to a map cluster and then add the cluster to the app
-        App.markers.addLayer(App.marker);
-        App.map.addLayer(App.markers);
+        markers.addLayer(marker);
+        //map.addLayer(markers);
     }
 }
 
@@ -32,25 +38,16 @@ function GeocodeCallback(result) {
 jQuery(document).ready(function($) {
     'use strict';
 
-    // Bing API key
-    App.credentials = "AnhYJU1pFXh6M5Qn4zg4htWejzdN5VKZ_c8CC0MQdsCNHaxz-JzNbmwfsMyx3bDq";
-
     // Function to fetch geocode
     function geocode(address) {
         // Build the REST URL to fetch
         // Quick regex to add '+' as spaces
-        App.url = "http://dev.virtualearth.net/REST/v1/Locations?query=" + address + "+Vancouver+BC+Canada&output=json&jsonp=GeocodeCallback&key=" + App.credentials;
-        return App.url;
+        var url = "http://dev.virtualearth.net/REST/v1/Locations?query=" + address + "+Vancouver+BC+Canada&output=json&jsonp=GeocodeCallback&key=" + Data.bingKey;
+        return url;
     }
 
     function fetchCoordinates(value) {
-        // Use Bing API to grab coordinates of locations
-        // The geocode function is used here
-        // GeocodeCallback is the response Bing sends back
-        $.ajax({
-            url: geocode(value),
-            dataType: 'jsonp'
-        });
+        $.ajax({ url: geocode(value), dataType: 'jsonp' });
     }
 
     function createHandlebarTemplate(data) {
@@ -61,7 +58,7 @@ jQuery(document).ready(function($) {
     }
 
     // Grab CSV
-    App.ds = new Miso.Dataset({
+    var ds = new Miso.Dataset({
         // The actual url is a FTP link so AJAX won't cut it due to CORS issues
         // ftp://webftp.vancouver.ca/opendata/csv/RentalStandardsCurrentIssues.csv
 
@@ -71,48 +68,42 @@ jQuery(document).ready(function($) {
         delimiter: ','
     });
 
-    App.ds.fetch({
+    ds.fetch({
         success: function() {
-            App.totalUnits = 0;
-            App.totalOutstanding = 0;
-
             // Remove null values
             this.remove(function(row) { return row.STREET === null });
 
             // iterate over rows
             this.each(function(row) {
                 // Populate Averages
-                App.totalUnits += row.TOTALUNITS
-                App.totalOutstanding += row.TOTALOUTSTANDING
+                Data.totalUnits += row.TOTALUNITS
+                Data.totalOutstanding += row.TOTALOUTSTANDING
 
                 // Fetch coordinates for map and add values to table template
                 fetchCoordinates(row.STREETNUMBER + "+" + row.STREET)
                 createHandlebarTemplate(row)
             }); // $.each
 
-            App.averageUnit = App.totalUnits / this.length;
-            App.averageOutstanding = App.totalOutstanding / this.length;
-            console.log(App.averageUnit);
+            Data.averageUnit = Data.totalUnits / this.length;
+            Data.averageOutstanding = Data.totalOutstanding / this.length;
 
-            // Charts
-            var data = [
+            // Chart
+            new Chart($("#chart").get(0).getContext("2d")).Pie([
                 {
-                    value: App.averageUnit,
+                    value: Data.averageUnit,
                     color: "#F7464A"
                 },
                 {
-                    value: App.averageOutstanding,
+                    value: Data.averageOutstanding,
                     color: "#46BFBD"
                 }
-            ];
-            new Chart($("#chart").get(0).getContext("2d")).Pie(data);
-            swerve = this;
+            ]);
         } // success
     });
 
     // Setup Leaflet map
-    App.layer = new L.StamenTileLayer("toner");
-    App.map = new L.Map("map", {
+    var layer = new L.StamenTileLayer("toner");
+    var map = new L.Map("map", {
         // Vancouver
         center: new L.LatLng(49.261226, -123.113927),
         zoom: 12,
@@ -125,18 +116,18 @@ jQuery(document).ready(function($) {
     });
 
     // Load map to DOM
-    App.map.addLayer(App.layer);
+    map.addLayer(layer);
 
     // Control
-    new L.Control.Zoom({position: "topright"}).addTo(App.map);
+    new L.Control.Zoom({position: "topright"}).addTo(map);
 
     new L.Control.GeoSearch({
         provider: new L.GeoSearch.Provider.Bing({
-            key: App.credentials
+            key: Data.bingKey
         }),
         country: 'Canada',
         zoomLevel: 16
-    }).addTo(App.map);
+    }).addTo(map);
 
     // Handlebar helper for some math
     Handlebars.registerHelper('percentage', function(value, divisor) {
