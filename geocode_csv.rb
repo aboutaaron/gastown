3f253c66cc3aff0c411e133bcd8b34e7175b48cc
aboutaaron <aaron.colby.williams@gmail.com>
2013-06-03 20:14:01 -0700

    enabled writing

--- geocode_csv.rb
+++ geocode_csv.rb
@@ -6,7 +6,7 @@ Geocoder.configure(
   :timeout => 5
 )
 
-csv = CSV.open("app/data/RentalStandardsCurrentIssues.csv", :headers => :first_row)
+csv = CSV.open("app/data/RentalStandardsCurrentIssues.csv", :headers => :first_row, "w+")
 
 csv.each do |row|
   full_address = "#{row[0]} #{row[1]}, Vancouver, BC, Canada"
