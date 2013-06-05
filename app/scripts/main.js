b5e1d2b57bf234ec856747f969d27e12512a011e
aboutaaron <aaron.colby.williams@gmail.com>
2013-06-05 10:57:19 -0700

    first round of jshint tweaks

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,24 +1,27 @@
-var Gastown = Gastown || {}, s, p;
+/*global L:false, Handlebars:false, $:false, jQuery:false */
+
+var Gastown = Gastown || {}, s;
 
 Gastown = {
     config: {
         defaultZoomLevel: 16,
-        map: new L.Map("map", {
-                // Vancouver
-                center: new L.LatLng(49.261226, -123.113927),
-                zoom: 12,
+        map: new L.Map('map', {
+            // Vancouver
+            center: new L.LatLng(49.261226, -123.113927),
+            zoom: 12,
 
-                // Options
-                scrollWheelZoom: false,
-                touchZoom: false,
-                doubleClickZoom: false,
-                zoomControl: false
-            }),
+            // Options
+            scrollWheelZoom: false,
+            touchZoom: false,
+            doubleClickZoom: false,
+            zoomControl: false
+        }),
         data: 0,
         helpers: Handlebars.registerHelper('percentage', function(value, divisor) {
+            "use strict";
             return ((value / divisor) * 100).toFixed(1);
         }),
-        csv: "data/2013-06-03-rental-standards.csv",
+        csv: 'data/2013-06-03-rental-standards.csv',
         cluster: new L.MarkerClusterGroup({
             // Change on.click zoom value
             showCoverageOnHover: false,
@@ -27,7 +30,8 @@ Gastown = {
     },
 
     init: function() {
-        console.log("Pew pew pew pew!")
+        "use strict";
+
         // pull in config
         s = this.config;
 
@@ -37,9 +41,11 @@ Gastown = {
     },
 
     createMap: function() {
+        "use strict";
+
         // Setup Leaflet map
-        var layer = new L.StamenTileLayer("toner");
-        var zoomLayer = new L.Control.Zoom({position: "topright"});
+        var layer = new L.StamenTileLayer('toner');
+        var zoomLayer = new L.Control.Zoom({position: 'topright'});
         var geoSearchLayer = new L.Control.GeoSearch({
             provider: new L.GeoSearch.Provider.Google(),
             country: 'Canada',
@@ -58,14 +64,16 @@ Gastown = {
     },
 
     extractCSV: function(url) {
+        "use strict";
+
         // Miso Dataset
         var ds = new Miso.Dataset({
             url : url,
-            delimiter : ","
+            delimiter : ','
         });
 
         ds.fetch({
-            error: function(error) { console.log(error) },
+            error: function(error) { console.log(error); },
             success: function() {
                 s.data = this;
                 Gastown.addMarkerToCluster(s.data);
@@ -74,15 +82,16 @@ Gastown = {
     },
 
     buildTemplate: function(data) {
+        "use strict";
         // Take an object and build a template from Handlebars
         // Handlebar template of data
-        var source = $("#rental-details-template").html();
+        var source = $('#rental-details-template').html();
         var template = Handlebars.compile(source);
-        $("#rental-details").html(template(data));
-        //console.log("Template built. Your data contains " + data.length + " values and the following methods (extracted from the CSV headers): " + data.columnNames());
+        $('#rental-details').html(template(data));
     },
 
     addMarkerToCluster: function(data) {
+        "use strict";
         // Take the JS Object
         // Iterate over each
         data.each(function(row) {
@@ -91,17 +100,17 @@ Gastown = {
             var m = new L.marker([row.LAT, row.LNG]);
             s.cluster.addLayer(m);
 
-            m.on("click", function() {
+            m.on('click', function() {
                 Gastown.buildTemplate(row);
             });
         });
 
         // After iteration, add cluster to map
-        s.map.addLayer(s.cluster)
+        s.map.addLayer(s.cluster);
     }
-}
+};
 
-jQuery(document).ready(function($) {
+jQuery(document).ready(function() {
     "use strict";
     Gastown.init();
 });
